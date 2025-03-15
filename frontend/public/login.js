62ad650e6dc150c972b44096d4cd8c8fcc9287f5
clara-hct02 <clarahct1102@gmail.com>
2025-03-15 11:29:18 -0700

    Basic login page

---
+++ frontend/public/login.js
@@ -0,0 +1,20 @@
+async function login(event) {
+    event.preventDefault();
+
+    var user = document.getElementById('name').value;
+    var password = document.getElementById('password').value;
+
+    console.log(user);
+    console.log(password);
+
+    const response = await fetch("/login", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: user, password: password }),
+    });
+    const data = await response.json();
+}
+
+window.onload = function() {
+    document.getElementById("login").addEventListener("submit", login);
+}
