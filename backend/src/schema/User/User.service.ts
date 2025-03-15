de4bd9e170c7847b53eed50dbfa92700bda6a948
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2025-03-15 16:19:44 -0700

    more fixes...

--- backend/src/schema/User/User.service.ts
+++ backend/src/schema/User/User.service.ts
@@ -16,29 +16,25 @@ export class UserService {
   }
 
   async findAll(): Promise<UserDocument[]> {
-    return this.userModel
-      .find()
-      .select('-password')
-      .populate('CourseList')
-      .exec();
+    return this.userModel.find().select('-password').exec();
   }
 
   async findById(id: string): Promise<UserDocument | null> {
-    return this.userModel.findById(id).populate('CourseList').exec();
+    return this.userModel.findById(id).exec();
   }
 
   async findByEmail(email: string): Promise<UserDocument | null> {
-    return this.userModel.findOne({ email }).populate('CourseList').exec();
+    return this.userModel.findOne({ email }).exec();
   }
 
   async update(id: string, user: User): Promise<UserDocument | null> {
     return this.userModel
       .findByIdAndUpdate(id, user, { new: true })
-      .populate('CourseList')
+
       .exec();
   }
 
   async remove(id: string): Promise<UserDocument | null> {
-    return this.userModel.findByIdAndDelete(id).populate('CourseList').exec();
+    return this.userModel.findByIdAndDelete(id).exec();
   }
 }
