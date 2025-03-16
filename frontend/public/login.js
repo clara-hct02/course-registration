a10d3a5c9a4a59ebcf5a2e97e8be36834214f4e9
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2025-03-15 20:06:25 -0700

    setup login flow and store session

--- frontend/public/login.js
+++ frontend/public/login.js
@@ -1,18 +1,32 @@
 async function login(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var user = document.getElementById('name').value;
-    var password = document.getElementById('password').value;
+  var user = document.getElementById("name").value;
+  var password = document.getElementById("password").value;
 
-    const response = await fetch("http://localhost:3000/users/login", {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: user, password: password }),
-    });
-    const data = await response.json();
+  const response = await fetch("http://localhost:3000/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: user, password: password }),
+  });
+  const data = await response.json();
+  console.log(data);
+  if (response.status == 201) {
     window.location.href = "/";
+    sessionStorage.setItem("id", data["_id"]);
+    sessionStorage.setItem("username", data["username"]);
+    sessionStorage.setItem("email", data["email"]);
+    sessionStorage.setItem(
+      "registrations",
+      JSON.stringify(data["registrations"])
+    );
+    sessionStorage.setItem("worklists", JSON.stringify(data["worklists"]));
+  } else {
+    // do nothing
+  }
+  //   window.location.href = "/";
 }
 
-window.onload = function() {
-    document.getElementById("login").addEventListener("submit", login);
-}
+window.onload = function () {
+  document.getElementById("login").addEventListener("submit", login);
+};
